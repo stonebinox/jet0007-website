0b7fe6a80f86d2d09e45450cabe397169a1125c0
Anoop Santhanam <anoop.santhanam@gmail.com>
2025-04-28 14:01:29 +0530

    config changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <a href="mailto:info@jet007carwash.com" className={styles.contactLink}>
+      <a href="mailto:contact@jet007.com" className={styles.contactLink}>
         Contact Us
       </a>
     </footer>
